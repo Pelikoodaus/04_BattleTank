2b572434a1d276dd0b9deea0ac0458857946cab9
Antti Toivonen <antti.toivonen@hotmail.com>
2018-07-16 11:27:05 +0300

    BT35 Using SpawnActor<>() to spawn

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Tank.h"
+#include "TankBarrel.h"
+#include "Projectile.h"
 #include "Engine/World.h"
 #include "TankAimingComponent.h"
 
@@ -19,6 +21,7 @@ ATank::ATank()
 void ATank::SetBarrelReference(UTankBarrel* BarrelToSet)
 {
 	TankAimingComponent->SetBarrelReference(BarrelToSet);
+	Barrel = BarrelToSet;
 }
 
 void ATank::SetTurretReference(UTankTurret* TurretToSet)
@@ -49,4 +52,15 @@ void ATank::Fire()
 {
 	auto Time = GetWorld()->GetTimeSeconds();
 	UE_LOG(LogTemp, Warning, TEXT("%f: Tank fires"), Time);
+
+	if (!Barrel) { return; }
+
+	GetWorld()->SpawnActor<AProjectile>(
+		ProjectileBlueprint,
+		Barrel->GetSocketLocation(FName("Projectile")),
+		Barrel->GetSocketRotation(FName("Projectile"))
+
+	);
+
 }
+
--- README.md
+++ README.md
@@ -38,3 +38,4 @@ open world head to head tank game [Link](https://github.com/Pelikoodaus/04_Battl
 * BT32 CHALLENGE Turret Rotation
 * BT33 Turret Rotation part 2
 * BT34 Setting up projectiles
+* BT35 Using SpawnActor<>() to spawn
