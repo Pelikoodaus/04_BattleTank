c53b3dee27429d45445457aa31a08d9d9d3e9625
Antti Toivonen <antti.toivonen@hotmail.com>
2018-07-18 11:09:20 +0300

    BT41 ApplyForceAtLocation() in Action

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -9,5 +9,10 @@ void UTankTrack::SetThrottle(float Throttle)
 	// auto Time = GetWorld()->GetTimeSeconds();
 	auto Name = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%s Throttles: %f"), *Name, Throttle);
+
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
 
--- README.md
+++ README.md
@@ -44,3 +44,4 @@ open world head to head tank game [Link](https://github.com/Pelikoodaus/04_Battl
 * BT38 EditAnywhere vs EditDefaultsOnly
 * BT39 Adding a Quit button
 * BT40 Setup track throttles
+* BT41 ApplyForceAtLocation() in Action
